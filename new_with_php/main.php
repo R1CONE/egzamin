adda96b76946684307ebc3c4373389a6af3aa96d
R1CONE <116386177+R1CONE@users.noreply.github.com>
2025-11-09 18:56:29 +0100

    Create main.php

---
+++ new_with_php/main.php
@@ -0,0 +1,23 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Strona medyczna</title>
+    <link rel = 'stylesheet' href = 'style.css'>
+
+
+</head>
+<body>
+    <img src="C:\xampp\htdocs\s_i\obraz1.jpg">
+    <section id = "gora"> asdasdas</section>
+    <section id = "gora2"> asdasdas</section>
+    <section id = "srodek1" >asdasdasd</section>
+    <section id = "srodek2" >asdasd</section>
+    <section id = "stopka" >asdasdd</section>
+
+</body>
+</html>
