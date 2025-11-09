81ffd68429891046698d4e40de34fcb40fdfae25
R1CONE <116386177+R1CONE@users.noreply.github.com>
2025-11-09 19:07:39 +0100

    Update main.php

--- new_with_php/main.php
+++ new_with_php/main.php
@@ -1,23 +1,24 @@
-<?php
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Strona medyczna</title>
-    <link rel = 'stylesheet' href = 'style.css'>
-
-
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <img src="C:\xampp\htdocs\s_i\obraz1.jpg">
-    <section id = "gora"> asdasdas</section>
-    <section id = "gora2"> asdasdas</section>
-    <section id = "srodek1" >asdasdasd</section>
-    <section id = "srodek2" >asdasd</section>
-    <section id = "stopka" >asdasdd</section>
+
+    <div class="top">
+        <section id="gora">asdasdas</section>
+        <section id="gora2"></section>
+    </div>
+
+    <div class="middle">
+        <section id="srodek1">asdasdasd</section>
+        <section id="srodek2">asdasd</section>
+    </div>
+
+    <section id="stopka">asdasdd</section>
 
 </body>
 </html>
