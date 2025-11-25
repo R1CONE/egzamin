2500bc0742b85923496a22b96d9d5fe565d4d3a7
R1CONE <116386177+R1CONE@users.noreply.github.com>
2025-11-25 10:59:24 +0100

    Update main_stycz_2025.php

--- Egzamin zawodowy INF03 2022 styczeń/main_stycz_2025.php
+++ Egzamin zawodowy INF03 2022 styczeń/main_stycz_2025.php
@@ -1,4 +1,24 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Strona medyczna</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
 
-?>
+    <div class="top">
+        <section id="gora">asdasdas</section>
+        <section id="gora2"></section>
+    </div>
 
+    <div class="middle">
+        <section id="srodek1">asdasdasd</section>
+        <section id="srodek2">asdasd</section>
+    </div>
+    
+    <section id="stopka">asdasdd</section>
+
+</body>
+</html>
