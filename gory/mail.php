35a1f215c66d1e7b7136dc477dfdc1243b4edbbc
R1CONE <116386177+R1CONE@users.noreply.github.com>
2025-12-08 10:42:16 +0100

    Update mail.php

--- gory/mail.php
+++ gory/mail.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "medica";
+$dbname = "baza";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
@@ -31,7 +31,33 @@ if ($conn->connect_error) {
     <section id = "id1">Klub zdobywców gór polskich</section>
     <section id = "id2"></section>
     <section id = "id3"><img id = "zdjecie" src = "photo.webp"></section>
-    <section id = "id4"></section>
+    <section id = "id4">
+
+    <table border="1">
+  <thead>
+    <tr>
+      <td>Пол           </td>
+      <td>Колличество   </td>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>Мальчики      </td>
+      <td>3</td>
+    </tr>
+    <tr>
+      <td>Девочки       </td>
+      <td>4</td>
+    </tr>
+  </tbody>
+  <tfoot>
+    <tr>
+      <td colspan = "2">Итог: 5</td>
+    </tr>
+  </tfoot>
+</table>
+
+    </section>
     <section id = "id5"></section>
 
 
