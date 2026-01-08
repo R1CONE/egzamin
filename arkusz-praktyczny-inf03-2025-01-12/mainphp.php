0067f8ed4017b5f15693b57eb58c56a9a49f7fe1
R1CONE <116386177+R1CONE@users.noreply.github.com>
2026-01-08 14:09:18 +0100

    Update mainphp.php

--- arkusz-praktyczny-inf03-2025-01-12/mainphp.php
+++ arkusz-praktyczny-inf03-2025-01-12/mainphp.php
@@ -1,1 +1,25 @@
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <?php 
+    
+
+
+$conn = new mysqli(hostname: "localhost",username: "root",password: "",database: "piekarnia");
+
+if ($conn->connect_error) {
+    echo "Błąd połączenia: " . $conn->connect_error;
+} else {
+}
+
+    
+    ?>
+</body>
+</html>
